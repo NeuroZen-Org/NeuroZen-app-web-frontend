3479af2934e6af03450c18778940f870a63b0920
Yonyu Gamer <yonyugmer8@gmail.com>
2025-10-08 13:21:34 -0500

    Change test

--- src/services/HttpClient.js
+++ src/services/HttpClient.js
@@ -12,7 +12,7 @@
  */
 export class HttpClient {
   /**
-   * Constructor del cliente HTTP
+   * Constructor del cliente HTTP oa
    * @param {string} baseURL - URL base de la API (por defecto: http://localhost:3002)
    */
   constructor(baseURL = 'http://localhost:3002') {
