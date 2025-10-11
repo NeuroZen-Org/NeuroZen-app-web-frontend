08a6f330e8663b3381dd516ec1e199cd28881132
Ax3LJnr <u20231g054@upc.edu.pe>
2025-10-10 20:17:52 -0500

    feat: replace svg logo with image in header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,10 +5,7 @@
       <div class="header-brand">
         <router-link to="/dashboard" class="brand-link">
           <div class="logo">
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="16" cy="16" r="16" fill="#4F46E5"/>
-              <path d="M12 20L20 12M12 12L20 20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
+            <img src="/neurozen1_logo.png" alt="NeuroZen Logo" width="47" height="47" />
           </div>
           <span class="brand-name">{{ $t('app.name') }}</span>
         </router-link>
